62149a01ed1218c15044fa15fd1c0037a91fd524
iosiflagranj <38509148+iosiflagranj@users.noreply.github.com>
2018-04-19 16:18:26 +0300

    Update hello.c
    fixed bug

--- hello.c
+++ hello.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
+//Am formatat codul si am fixat un bug -mih
 
-
-
+#include <stdio.h
 
 int main(int argc,char *argv[]){
-if(argc!=2){
+	if(argc!=2){
+		printf("USAGE %s name \n",argv[0]);
+		return -1;
+	}
 
-
-	printf("USAGE %s name \n",argv[0]);
-	return -1;
-}
-
-
-
-printf("hello, %s!\n,argv[1]);
-return 0;
+	printf("hello, %s!\n",argv[1]);
+	return 0;
 }
 
 
